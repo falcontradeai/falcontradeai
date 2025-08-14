8c3da3c2f989c7da57025eb9453bc5a0bf379b4c
falcontradeai <aifalcontrade@gmail.com>
2025-08-14 15:18:47 +0400

    Add Tailwind CSS with dark mode and theme toggle

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,9 +1,14 @@
+import '../styles/globals.css';
 import { AuthProvider } from '../contexts/AuthContext';
+import ThemeToggle from '../components/ThemeToggle';
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
-      <Component {...pageProps} />
+      <div className="min-h-screen bg-white dark:bg-gray-900 text-black dark:text-white">
+        <ThemeToggle />
+        <Component {...pageProps} />
+      </div>
     </AuthProvider>
   );
 }
