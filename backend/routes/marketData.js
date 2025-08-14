64a047ff30655fb49ee18081d1ae57761bc55269
falcontradeai <aifalcontrade@gmail.com>
2025-08-14 14:52:44 +0400

    Add market data endpoints with moving average forecast

--- backend/routes/marketData.js
+++ backend/routes/marketData.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const auth = require('../middleware/auth');
+const { isAdmin } = require('../middleware/roles');
+const marketDataController = require('../controllers/marketData');
 
 const router = express.Router();
 
-router.get('/', auth, (req, res) => {
-  res.json({ message: 'Market data endpoint' });
-});
+router.get('/:commodity', auth, marketDataController.getMarketData);
+router.post('/:commodity', auth, isAdmin, marketDataController.addMarketData);
+router.put('/:commodity', auth, isAdmin, marketDataController.updateMarketData);
 
 module.exports = router;
